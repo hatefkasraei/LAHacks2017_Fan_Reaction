413e30446c5b7380f57ba7c3677d3a77c798a134
Hatef Kasraei <hatefkasraei@gmail.com>
2017-04-01 13:23:30 -0700

    emotions class is better now

--- FanReplay/Emotions.swift
+++ FanReplay/Emotions.swift
@@ -10,6 +10,17 @@ import Foundation
 
 class Emotions
 {
+	var anger : Double = 0;
+	var fear : Double = 0;
+	var surprise : Double = 0;
+	var contempt : Double = 0;
+	var disgust : Double = 0;
+	var happiness : Double = 0;
+	var neutral : Double = 0;
+	var sadness : Double = 0;
+	
+	var peoplePresent : Double = 0;
+	
 	func readEmotions(json : JSONSerialization)
 	{
 		for face in json.mutableArrayValue(forKey: "scores")
@@ -18,8 +29,57 @@ class Emotions
 			
 			for (emotion, score) in emotions!
 			{
-				print("\(emotion) : \(score)\n");
+				switch emotion {
+					case "anger":
+						self.anger += score;
+					case "fear":
+						self.fear += score;
+					case "surprise":
+						self.surprise += score;
+					case "contempt":
+						self.contempt += score;
+					case "disgust":
+						self.disgust += score;
+					case "happiness":
+						self.happiness += score;
+					case "neutral":
+						self.neutral += score;
+					case "sadness":
+						self.sadness += score;
+				default:
+					continue
+				}
 			}
+			self.peoplePresent += 1;
 		}
+		_balanceData();
+	}
+	
+	func consolePrint()
+	{
+		print("-- -- -- Average Emotions in Picture -- -- --");
+		print("Total Faces in this picture: \(Int(self.peoplePresent))");
+		print("anger : \(Int(self.anger * 100))%");
+		print("fear : \(Int(self.fear * 100))%");
+		print("surprise : \(Int(self.surprise * 100))%");
+		print("contempt : \(Int(self.contempt * 100))%");
+		print("disgust : \(Int(self.disgust * 100))%");
+		print("happiness : \(Int(self.happiness * 100))%");
+		print("neutral : \(Int(self.neutral * 100))%");
+		print("sadness : \(Int(self.sadness * 100))%");
+		print("-- -- -- -- -- -- --  -- -- -- -- -- -- -- --");
+	}
+	
+	private func _balanceData()
+	{
+		self.anger = self.anger/peoplePresent;
+		self.fear = self.anger/peoplePresent;
+		self.surprise = self.surprise/peoplePresent;
+		self.contempt = self.contempt/peoplePresent;
+		self.disgust = self.disgust/peoplePresent;
+		self.happiness = self.happiness/peoplePresent;
+		self.neutral = self.neutral/peoplePresent;
+		self.sadness = self.sadness/peoplePresent;
+		
 	}
 }
