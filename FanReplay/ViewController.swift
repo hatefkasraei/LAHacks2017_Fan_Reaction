71fc68290cdc953c73460205c05c840462ac27d4
Hatef Kasraei <hatefkasraei@gmail.com>
2017-04-01 05:58:14 -0700

    Initial Commit

---
+++ FanReplay/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FanReplay
+//
+//  Created by Hatef Kasraei on 4/1/17.
+//  Copyright © 2017 The Possimpibles. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
