d5014519ba8859c224ed1188e7d124807bf80537
Hatef Kasraei <hatefkasraei@gmail.com>
2017-04-02 01:46:47 -0700

    improved grabframe function

--- AVHandlers.swift
+++ AVHandlers.swift
@@ -17,18 +17,12 @@ import CoreGraphics
 
 func grabframe(videoPath : String, timeInSeconds : Double, destinationPath : String)
 {
-	var movie = AVAsset(url: URL(fileURLWithPath: videoPath))
-	var track = movie.tracks(withMediaType: "vide").first!
-	let imagegenerator = AVAssetImageGenerator(asset: movie);
-	var time = CMTime(seconds: timeInSeconds, preferredTimescale: 100);
+	let imagegenerator = AVAssetImageGenerator(asset: AVAsset(url: URL(fileURLWithPath: videoPath)));
 	
-	var capturedImage : NSImage;
-	
-	if let imageref = try? imagegenerator.copyCGImage(at: time, actualTime: nil)
+	if let imageref = try? imagegenerator.copyCGImage(at: CMTime(seconds: timeInSeconds, preferredTimescale: 100), actualTime: nil)
 	{
-		capturedImage = NSImage(cgImage: imageref, size: NSSize(width: 100, height: 100));
-		var newRep = NSBitmapImageRep(cgImage: imageref);
-		var data = newRep.representation(using: NSPNGFileType, properties: [String : Any]());
+		let data = NSBitmapImageRep(cgImage: imageref).representation(using: NSPNGFileType, properties: [String : Any]());
 		try? data?.write(to: URL(fileURLWithPath: destinationPath));
 	};
 }
+
--- FanReplay/AppDelegate.swift
+++ FanReplay/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
 		// Insert code here to initialize your application
 		
-		grabframe(videoPath: "/Users/hatef/Desktop/messi.mp4", timeInSeconds: 2.0, destinationPath: "/Users/hatef/Desktop/test.png");
+		grabframe(videoPath: "/Users/hatef/Desktop/messi.mp4", timeInSeconds: 10.42, destinationPath: "/Users/hatef/Desktop/test.png");
 		
 	}
 
