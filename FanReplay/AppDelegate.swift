47fc18c84c29673d0da1afcaf82609efc4d1f514
Hatef Kasraei <hatefkasraei@gmail.com>
2017-04-02 05:27:08 -0700

    time range saver works

--- FanReplay/AppDelegate.swift
+++ FanReplay/AppDelegate.swift
@@ -15,6 +15,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
 		// Insert code here to initialize your application
+		//		grabFrameAndSaveOnDisk(videoPath: "/Users/hatef/Desktop/fan/adidas.mp4", timeInSeconds: 43.02, destinationPath: "/Users/hatef/Desktop/fan/outputed/adidas.jpg");
+		
+		let api = API();
+		let emotion = Emotions();
+		emotion.readEmotions(json: api.getJSONwithIMAGE(grabFrameInJPEG(videoPath: "/Users/hatef/Desktop/fan/adidas.mp4" , timeInSeconds: 43.02)));
+		emotion.consolePrint()
+		
+		playBackWithinTimeRange(videoPath: "/Users/hatef/Desktop/fan/adidas.mp4", start: 1.0, end: 23.03, destinationPath: "/Users/hatef/Desktop/fan/outputed/saved.mp4");
+		
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
