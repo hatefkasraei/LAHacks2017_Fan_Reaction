90c3d56652afbc7e170d7787887e673b84ba4eba
dannypenguin <DannyPengXD@gmail.com>
2017-04-01 22:41:22 -0700

    Initial Commit

---
+++ FanReplay/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FanReplay
+//
+//  Created by Hatef Kasraei on 4/1/17.
+//  Copyright © 2017 The Possimpibles. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
